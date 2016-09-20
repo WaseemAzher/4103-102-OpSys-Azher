d8ea98f38f5a892bbad1d0e5ff7f7a792f77c6c7
waseemazher <waseemazher89@gmail.com>
2016-09-20 11:59:22 -0400

    modifying homework2

--- assignments/homework-01/versionb.sh
+++ assignments/homework-01/versionb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Myfile=$(basename "$1" | cut -d. -f1)
-Curdate=$(date +"%Y-%m-%d")
+Curdate="_$(date +"%Y-%m-%d")"
 NewName=$Myfile$Curdate
 Newfile=${1/$Myfile/$NewName}
 cp $1 $Newfile
